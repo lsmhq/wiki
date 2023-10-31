6f5dfe978fbef70ccf890b2d1a96ba3887e70fcd
daitianfang <784433957@qq.com>
2023-10-31 18:02:11 +0800

    vuepress init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "Subata",
+  description: "Subata",
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
